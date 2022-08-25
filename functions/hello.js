a9edf65c9b106af6ac3e461ee7a45b9d998220d5
Magesh_Kumar <48789795+mageshkumar1598@users.noreply.github.com>
2022-08-25 18:29:46 +0530

    cloudflare functions

--- functions/hello.js
+++ functions/hello.js
@@ -2,5 +2,5 @@ export async function onRequestGet(request) {
   // ...
   const counter = await request.env.store.get("visitor");
   await request.env.store.put("visitor", parseInt(counter) + 1);
-  return new Response({ count: parseInt(counter) + 1 });
+  return new Response(JSON.stringify({ count: parseInt(counter) + 1 }));
 }
